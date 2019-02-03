a33256bebbb44cba5e4b7b04259346af8716c1f3
Rodrigo Ruas <rodrigoruas2@gmail.com>
2019-02-03 18:39:56 +0000

    Config PagSeguro to Production
    - Add production token
    - Add production URL

--- config/initializers/pagseguro.rb
+++ config/initializers/pagseguro.rb
@@ -1,6 +1,12 @@
 PagSeguro.configure do |config|
-  config.token       = "033A26CA5C6643519A2687DD3FCB1383"
-  config.email       = "robertaazevedo@amparando.com.br"
-  config.environment = :sandbox # ou :production. O padrão é production.
+  if Rails.env.production?
+    config.token     = "6C1FB8FE551E4504B2C68BA79D8209EC"
+    config.environment =  :production
+  else
+    config.environment =  :sandbox
+    config.token     = "033A26CA5C6643519A2687DD3FCB1383"
+  end
   config.encoding    = "UTF-8" # ou ISO-8859-1. O padrão é UTF-8.
+  config.email       = "robertaazevedo@amparando.com.br"
 end
+
