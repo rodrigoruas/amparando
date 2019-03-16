48f2ab34ef755a800307a5fa50a1cf6ef90e90e9
Rodrigo Ruas <rodrigoruas2@gmail.com>
2019-03-16 10:25:24 -0300

    Admin Link On Navbar
    - Add Admin link on Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'unique_donations/new'
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 
   root to: 'pages#home'
   get "/sobre-nos", to: 'pages#about', as: :about
@@ -23,8 +25,6 @@ Rails.application.routes.draw do
     resources :payments, only: [:new, :create]
   end
 
-  devise_for :users do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
