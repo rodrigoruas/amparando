b3b6d6f7a9475c3762e0dddde9a3791a746d30f7
Rodrigo Ruas <rodrigoruas2@gmail.com>
2019-08-13 19:04:22 +0100

    Update Stripe Redirect

--- config/initializers/pagseguro.rb
+++ config/initializers/pagseguro.rb
@@ -1,17 +1,13 @@
-PG_PRODUCTION_TOKEN = "6C1FB8FE551E4504B2C68BA79D8209EC"
-PG_SANDBOX_TOKEN = "033A26CA5C6643519A2687DD3FCB1383"
-PG_EMAIL = "robertaazevedo@amparando.com.br"
 
-
-PagSeguro.configure do |config|
-  if Rails.env.production?
-    config.token     = PG_PRODUCTION_TOKEN
-    config.environment =  :production
-  else
-    config.environment =  :sandbox
-    config.token     = PG_SANDBOX_TOKEN
-  end
-  config.encoding    = "UTF-8" # ou ISO-8859-1. O padrão é UTF-8.
-  config.email       = PG_EMAIL
-end
+# PagSeguro.configure do |config|
+#   if Rails.env.production?
+#     config.token     = PG_PRODUCTION_TOKEN
+#     config.environment =  :production
+#   else
+#     config.environment =  :sandbox
+#     config.token     = PG_SANDBOX_TOKEN
+#   end
+#   config.encoding    = "UTF-8" # ou ISO-8859-1. O padrão é UTF-8.
+#   config.email       = PG_EMAIL
+# end
 
